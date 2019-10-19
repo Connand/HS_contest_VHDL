fa27f7fb4c3fdd06c175abb029b08c1b15e83187
Connand <30803780+Connand@users.noreply.github.com>
2019-10-19 21:10:41 +0800

    Add files via upload

---
+++ Cont_2018_2_vb/sim4_storer/storer_systerm/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(43, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 81)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "寄放行李"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Location = New System.Drawing.Point(43, 125)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(137, 81)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "取出行李"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button3.Location = New System.Drawing.Point(262, 38)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 81)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "1-1"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 1200
+        Me.SerialPort1.PortName = "COM5"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 50
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button4.Location = New System.Drawing.Point(358, 38)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(90, 81)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "1-2"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button5.Location = New System.Drawing.Point(454, 38)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(90, 81)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "1-3"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button6.Location = New System.Drawing.Point(550, 38)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(90, 81)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "1-4"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button7.Location = New System.Drawing.Point(262, 125)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(90, 109)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "2-1"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button8.Location = New System.Drawing.Point(358, 125)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(90, 109)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "2-2"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button9.Location = New System.Drawing.Point(454, 125)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(90, 109)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "2-3"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button10.Location = New System.Drawing.Point(550, 125)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(90, 109)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "2-4"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button11.Location = New System.Drawing.Point(262, 240)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(90, 149)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "3-1"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button12.Location = New System.Drawing.Point(358, 240)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(90, 149)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "3-2"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button13.Location = New System.Drawing.Point(454, 240)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(90, 149)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "3-3"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Font = New System.Drawing.Font("標楷體", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button14.Location = New System.Drawing.Point(550, 240)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(90, 149)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "3-4"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "寄物櫃管理系統"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+End Class
---
+++ Cont_2018_2_vb/sim4_storer/storer_systerm/Form1.vb
@@ -0,0 +1,214 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Public Class Form1
+    Dim UAR_pointer As Byte = 0
+    Dim UAT_pointer As Byte = 0
+    Dim UAT_data(50) As Byte
+    Dim UAR_data(50) As Byte
+    Dim command As Byte
+    Dim sel_num As Byte
+    Dim store_en As Boolean
+    Dim rent_take As Boolean
+    Dim storer_sel(24) As Boolean
+    Dim storer_use(24) As Boolean
+    Dim storers(24) As Button
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        With SerialPort1
+            .BaudRate = 1200 : .DataBits = 8 : .StopBits = StopBits.One : .PortName = "COM5" : .Open()
+        End With
+        For i = 0 To 24
+            storer_sel(i) = False
+            storer_use(i) = 0
+        Next
+        store_en = False
+        sel_num = 0
+        rent_take = 0 '寄物
+        command = &HFF
+        For i = 0 To 11
+            storers(i) = Controls("Button" & (i + 3))
+            storers(i).Font = New Font(storers(i).Font.FontFamily, 15, storers(i).Font.Style)
+        Next
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        SerialPort1.Close()
+    End Sub
+
+    '委派 更新畫面
+    Private Delegate Sub UpdateUICallBack(ByVal newText As String, ByVal c As Control)
+    Private Sub UpdateUI(ByVal newText As String, ByVal c As Control)
+        If Me.InvokeRequired() Then
+            Dim cb As New UpdateUICallBack(AddressOf UpdateUI)
+            Me.Invoke(cb, newText, c)
+        Else
+            c.Text = newText
+        End If
+    End Sub
+
+    Public Function Trans_to_fpga(data() As Byte, bytes As Byte)
+        For i = 0 To bytes - 1
+            SerialPort1.Write(data, i, 1)
+            Thread.Sleep(4) '間隔4ms避免太快接收不及
+        Next
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '寄物
+        store_en = True
+        '無法選取粉色 可選取綠色 選取黃色
+        rent_take = 0
+        UAT_data(0) = &HCA '寄物
+        Trans_to_fpga(UAT_data, 1)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '取物
+        store_en = True
+        '已租紅 未租綠 選取黃色
+        rent_take = 1
+        UAT_data(0) = &HCB '取物
+        Trans_to_fpga(UAT_data, 1)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim buff As Byte
+        Dim storer_num As Integer
+        If Timer1.Interval = 1000 Then '初始到待機
+            Button1.BackColor = Color.LightGray
+            Button2.BackColor = Color.LightGray
+            For i = 0 To 11
+                storers(i).BackColor = Color.LightGray
+            Next
+            Timer1.Interval = 10
+        End If
+        If SerialPort1.BytesToRead > 0 Then
+            buff = SerialPort1.ReadByte() '讀取資料--------------------------------------------
+            If buff >= &HA0 Then '如果是指令
+                UAR_data(0) = buff
+                UAR_pointer = 1
+            Else
+                UAR_data(UAR_pointer) = buff
+                UAR_pointer = UAR_pointer + 1
+            End If
+            Select Case UAR_data(0)
+                Case &HEE 'EE RESET
+                    Button1.BackColor = Color.LightGray
+                    Button2.BackColor = Color.LightGray
+                    For i = 0 To 11
+                        storers(i).BackColor = Color.LightGray
+                    Next
+                    Button1.Enabled = True
+                    Button2.Enabled = True
+                    store_en = False
+                    For i = 0 To 12
+                        storer_sel(i) = False
+                        storer_use(i) = 0
+                    Next
+                    sel_num = 0
+                    Timer1.Interval = 1000
+                Case &HAC 'AC 使用狀況 櫃子狀態 前2編號 後2狀態(X0可用X1已用1X被選)
+                    If UAR_pointer > 24 Then
+                        For i = 0 To 11
+                            storer_num = 10 * (UAR_data(1 + 2 * i) \ 16) + (UAR_data(1 + 2 * i) Mod 16)
+                            storer_use(storer_num) = UAR_data(2 * (i + 1)) Mod 16 '01XX
+                            storer_sel(storer_num) = UAR_data(2 * (i + 1)) >> 4 '01XX
+                            If storer_use(storer_num) = True Then '已使用的櫃子
+                                If rent_take = 0 Then '寄物
+                                    storers(storer_num - 1).BackColor = Color.DarkGray
+                                Else '取物
+                                    storers(storer_num - 1).BackColor = Color.LightGray
+                                End If
+                            Else
+                                If rent_take = 0 Then '寄物
+                                    storers(storer_num - 1).BackColor = Color.LightGray
+                                Else '取物
+                                    storers(storer_num - 1).BackColor = Color.DarkGray
+                                End If
+                            End If
+                        Next
+                    End If
+                Case &HEA 'EA 操作結束
+                    Button1.BackColor = Color.Gray
+                    Button2.BackColor = Color.Gray
+                    For i = 0 To 11
+                        storers(i).BackColor = Color.Gray
+                    Next
+                    Button1.Enabled = True
+                    Button2.Enabled = True
+                    store_en = False
+                    sel_num = 0
+                    For i = 0 To 12
+                        storer_sel(i) = False
+                    Next
+                    If UAR_data(1) = &H99 Then
+                        Button1.Enabled = False
+                    End If
+                Case &HCA 'CA 寄物
+                    Button1.BackColor = Color.GreenYellow
+                    Button2.BackColor = Color.White
+                    Button2.Enabled = False
+
+                Case &HCB 'CB取物
+            End Select
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click, Button10.Click, Button11.Click, Button12.Click, Button13.Click, Button14.Click
+        If store_en Then
+            Dim sel As Byte = ((Int(Mid(sender.Text, 1, 1)) - 1) * 4 + Int(Mid(sender.Text, 3, 1)))
+            storer_sel(sel) = Not storer_sel(sel)
+            If storer_sel(sel) Then
+                If rent_take = 0 Then
+                    If sel_num > 7 Then
+                        storer_sel(sel) = Not storer_sel(sel)
+                    Else
+                        sel_num = sel_num + 1
+                    End If
+                Else
+                    If sel_num > 0 Then
+                        storer_sel(sel) = Not storer_sel(sel)
+                    Else
+                        sel_num = sel_num + 1
+                    End If
+                End If
+            Else
+                sel_num = sel_num - 1
+            End If
+            If rent_take = 0 Then '寄物
+                If storer_use(sel) Then '已使用的櫃子
+                    sender.BackColor = Color.DarkGray
+                Else
+                    If storer_sel(sel) Then
+                        sender.BackColor = Color.GreenYellow
+                    Else
+                        sender.BackColor = Color.LightGray
+                    End If
+                End If
+            Else '取物
+                If storer_use(sel) = False Then '沒有 使用的櫃子
+                    sender.BackColor = Color.GreenYellow
+                Else
+                    If storer_sel(sel) Then
+                        sender.BackColor = Color.GreenYellow
+                    Else
+                        sender.BackColor = Color.LightGray
+                    End If
+                End If
+            End If
+
+            UAT_data(0) = &HAB '選取情形
+            UAT_data(1) = ((sel \ 10) << 4) + (sel Mod 10)
+            If storer_sel(sel) Then
+                UAT_data(2) = &H10 '選擇
+            Else
+                UAT_data(2) = &H0 '沒選
+            End If
+            Trans_to_fpga(UAT_data, 3)
+        End If
+    End Sub
+
+
+End Class
